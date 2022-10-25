eeef50087da1d13e45fb1c87486c5be0240f3ca3
linus-jansson <contact@limpan.dev>
2022-10-25 17:24:50 +0200

    serverside props and useeffect session not same?

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 export default (props) => {
+    const [sessionData, setSessionData] = useState({});
+
+    useEffect(() => {
+        axios.get('/api/isloggedin').then((response) => {
+            console.log("use effect login check", response.data);
+            setSessionData(response.data);
+        });
+    })
+
     return (
         <>
             <h1>Very Secret site</h1>
 
-            <p>Session id: {props.sessiondata?.id}</p>
-            <p>logged in: {(props.sessiondata?.authed === true) ? true : false}</p>
+            <p>Session id: {sessionData?.id}</p>
+            <p>logged in: {(sessionData?.authed === true) ? "true" : "false"}</p>
+
+            <p>is GetServerSideProp id same as useeffect {(props.sessiondata?.id == sessionData?.id) ? "true" : "false"}</p>
 
             <Link href="/login">
                 <a>Login page</a>
@@ -16,7 +28,7 @@ export default (props) => {
 }
 
 export async function getServerSideProps(context) {
-    let response = await axios.get('http://localhost:8000/api/isloggedin');
+    let response = await axios.get('http://localhost:8000/api/isloggedin', { withCredentials: true });
     console.log(response.data);
     return {
         props: { sessiondata: response.data }, // will be passed to the page component as props
